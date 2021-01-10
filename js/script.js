c9dcea97ea8116c8053c722e8ed6d926b591343c
wxh <zwang252325@gmail.com>
2021-01-10 19:23:30 +0800

    finish page 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(function(){
     //思想学习tab显示
     $('#speach-nav-tabs a[href="#economic"]').tab('show');
-
+    //治国理政tab show
+    $('#manage-nav-tabs a[href="#conference"]').tab('show');
+    //思想解读 tab show
+    $('#interpretation-nav-tabs a[href="#specialist"]').tab('show');
 });
