d5157587badcaae3d1b5c55e958ac2625277adfd
wxh <zwang252325@gmail.com>
2021-01-11 16:04:59 +0800

    finish page3

--- js/script.js
+++ js/script.js
@@ -5,4 +5,10 @@ $(function(){
     $('#manage-nav-tabs a[href="#conference"]').tab('show');
     //思想解读 tab show
     $('#interpretation-nav-tabs a[href="#specialist"]').tab('show');
+
+    //牢记使命专题
+    //重要论述tab show
+    $('#edu-nav-tabs a[href="#comment"]').tab('show');
+
+    //保密知识库
 });
